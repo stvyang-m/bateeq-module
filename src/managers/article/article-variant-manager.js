c0cd6f9fb6384032c38751933c13f1e2406703a7
lioenel <kepin91@yahoo.co.id>
2016-07-22 11:13:09 +0700

    update validation and unit test for article variant
    update validation and unit test for article variant

--- src/managers/article/article-variant-manager.js
+++ src/managers/article/article-variant-manager.js
@@ -85,6 +85,8 @@ module.exports = class ArticleVariantManager {
 
     getById(id) {
         return new Promise((resolve, reject) => {
+            if (id === '')
+                resolve(null);
             var query = {
                 _id: new ObjectId(id),
                 _deleted: false
@@ -98,9 +100,11 @@ module.exports = class ArticleVariantManager {
                 });
         });
     }
- 
+
     getByIdOrDefault(id) {
         return new Promise((resolve, reject) => {
+            if (id === '')
+                resolve(null);
             var query = {
                 _id: new ObjectId(id),
                 _deleted: false
@@ -127,7 +131,7 @@ module.exports = class ArticleVariantManager {
                 });
         })
     }
-    
+
     getSingleOrDefaultByQuery(query) {
         return new Promise((resolve, reject) => {
             this.articleVariantCollection
@@ -196,13 +200,100 @@ module.exports = class ArticleVariantManager {
                 })
         });
     }
-
-
+ 
     _validate(articleVariant) {
+        var errors = {};
         return new Promise((resolve, reject) => {
             var valid = new ArticleVariant(articleVariant);
-            valid.stamp(this.user.username, 'manager');
-            resolve(valid);
+            // 1. begin: Declare promises.
+            var getArticleVariant = this.articleVariantCollection.singleOrDefault({
+                "$and": [{
+                    _id: {
+                        '$ne': new ObjectId(valid._id)
+                    }
+                }, {
+                        code: valid.code
+                    }]
+            });
+            // 1. end: Declare promises.
+
+            // 2. begin: Validation.
+            Promise.all([getArticleVariant])
+                .then(results => {
+                    var _articleVariant = results[0];
+
+                    if (!valid.code || valid.code == '')
+                        errors["code"] = "code is required";
+                    else if (_articleVariant)
+                        errors["code"] = "code already exists"; 
+                        
+                    if (!valid.name || valid.name == '')
+                        errors["name"] = "name is required";  
+                        
+                    if (!valid.size || valid.size == '')
+                        errors["size"] = "size is required";  
+                        
+                    if (valid.domesticCOGS == undefined || (valid.domesticCOGS && valid.domesticCOGS == '')) {
+                        errors["domesticCOGS"] = "domesticCOGS is required";
+                    }
+                    else if (parseInt(valid.domesticCOGS) < 0) {
+                        errors["domesticCOGS"] = "domesticCOGS must be greater with 0";
+                    }
+                    if (valid.domesticWholesale == undefined || (valid.domesticWholesale && valid.domesticWholesale == '')) {
+                        errors["domesticWholesale"] = "domesticWholesale is required";
+                    }
+                    else if (parseInt(valid.domesticWholesale) < 0) {
+                        errors["domesticWholesale"] = "domesticWholesale must be greater with 0";
+                    }
+                    if (valid.domesticRetail == undefined || (valid.domesticRetail && valid.domesticRetail == '')) {
+                        errors["domesticRetail"] = "domesticRetail is required";
+                    }
+                    else if (parseInt(valid.domesticRetail) < 0) {
+                        errors["domesticRetail"] = "domesticRetail must be greater with 0";
+                    } 
+                    if (valid.domesticSale == undefined || (valid.domesticSale && valid.domesticSale == '')) {
+                        errors["domesticSale"] = "domesticSale is required";
+                    }
+                    else if (parseInt(valid.domesticSale) < 0) {
+                        errors["domesticSale"] = "domesticSale must be greater with 0";
+                    } 
+                    if (valid.internationalCOGS == undefined || (valid.internationalCOGS && valid.internationalCOGS == '')) {
+                        errors["internationalCOGS"] = "internationalCOGS is required";
+                    }
+                    else if (parseInt(valid.internationalCOGS) < 0) {
+                        errors["internationalCOGS"] = "internationalCOGS must be greater with 0";
+                    }
+                    if (valid.internationalWholesale == undefined || (valid.internationalWholesale && valid.internationalWholesale == '')) {
+                        errors["internationalWholesale"] = "internationalWholesale is required";
+                    }
+                    else if (parseInt(valid.internationalWholesale) < 0) {
+                        errors["internationalWholesale"] = "internationalWholesale must be greater with 0";
+                    }
+                    if (valid.internationalRetail == undefined || (valid.internationalRetail && valid.internationalRetail == '')) {
+                        errors["internationalRetail"] = "internationalRetail is required";
+                    }
+                    else if (parseInt(valid.internationalRetail) < 0) {
+                        errors["internationalRetail"] = "internationalRetail must be greater with 0";
+                    }
+                    if (valid.internationalSale == undefined || (valid.internationalSale && valid.internationalSale == '')) {
+                        errors["internationalSale"] = "internationalSale is required";
+                    }
+                    else if (parseInt(valid.internationalSale) < 0) {
+                        errors["internationalSale"] = "internationalSale must be greater with 0";
+                    } 
+
+                    // 2c. begin: check if data has any error, reject if it has.
+                    for (var prop in errors) {
+                        var ValidationError = require('../../validation-error');
+                        reject(new ValidationError('data does not pass validation', errors));
+                    }
+
+                    valid.stamp(this.user.username, 'manager');
+                    resolve(valid);
+                })
+                .catch(e => {
+                    reject(e);
+                })
         });
     }
 };
