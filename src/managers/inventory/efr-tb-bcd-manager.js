ed1ad308e80414c4b30898a3e1ea24d23ab0c2d2
JanuitaM <januita.marpaung@moonlay.com>
2016-08-10 12:02:47 +0700

    remove module-seed and add generateCode in function create

--- src/managers/inventory/efr-tb-bcd-manager.js
+++ src/managers/inventory/efr-tb-bcd-manager.js
@@ -6,6 +6,7 @@ var ObjectId = require('mongodb').ObjectId;
 // internal deps
 require('mongodb-toolkit');
 var BateeqModels = require('bateeq-models');
+var generateCode = require('../../utils/code-generator');
 var map = BateeqModels.map;
 
 var TransferInDoc = BateeqModels.inventory.TransferInDoc;
@@ -33,9 +34,6 @@ module.exports = class FinishingTerimaBarcodeManager {
 
         var ModuleManager = require('../core/module-manager');
         this.moduleManager = new ModuleManager(db, user);
-
-        var ModuleSeedManager = require('../core/module-seed-manager');
-        this.moduleSeedManager = new ModuleSeedManager(db, user);
     }
 
     read(paging) {
@@ -63,7 +61,7 @@ module.exports = class FinishingTerimaBarcodeManager {
                 };
                 var $or = {
                     '$or': [filterCode]
-                }; 
+                };
                 query['$and'].push($or);
             }
 
@@ -146,41 +144,19 @@ module.exports = class FinishingTerimaBarcodeManager {
     create(transferInDoc) {
         return new Promise((resolve, reject) => {
             this._validate(transferInDoc)
-                .then(validTransferInDoc => { 
-                    var now = new Date();
-                    var year = now.getFullYear();
-                    var month = now.getMonth() + 1; 
-                    this.moduleSeedManager
-                        .getModuleSeed(moduleId, year, month)
-                        .then(moduleSeed => {
-                            var number = ++moduleSeed.seed;
-                            var zero = 4 - number.toString().length + 1;
-                            var runningNumber = Array(+(zero > 0 && zero)).join("0") + number; 
-                            zero = 2 - month.toString().length + 1;
-                            var formattedMonth = Array(+(zero > 0 && zero)).join("0") + month; 
-                            validTransferInDoc.code = `${runningNumber}/${moduleId}/${formattedMonth}/${year}`; 
-                            this.transferInDocManager.create(validTransferInDoc)
-                                .then(id => { 
-                                    this.moduleSeedManager
-                                        .update(moduleSeed)
-                                        .then(seedId => {
-                                            resolve(id);
-                                        })
-                                        .catch(e => {
-                                            reject(e);
-                                        })
-                                })
-                                .catch(e => {
-                                    reject(e);
-                                })
+                .then(validTransferInDoc => {
+                    validTransferInDoc.code = generateCode(moduleId);
+                    this.transferInDocManager.create(validTransferInDoc)
+                        .then(id => {
+                            resolve(id);
                         })
                         .catch(e => {
                             reject(e);
-                        });
+                        })
                 })
                 .catch(e => {
                     reject(e);
-                })
+                });
         });
     }
 
@@ -235,5 +211,5 @@ module.exports = class FinishingTerimaBarcodeManager {
                     reject(new Error(`Unable to load module:${moduleId}`));
                 });
         });
-    } 
+    }
 };
