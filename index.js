b5d9bd95a825dda061a9c4400dd04e845b9b9a84
JanuitaM <januita.marpaung@moonlay.com>
2016-08-11 11:30:36 +0700

    remove swg, bcd, exr and add manager in inventory

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ inventoryMap.set("transfer-in-doc", require('./src/managers/inventory/transfer-i
 inventoryMap.set("transfer-out-doc", require('./src/managers/inventory/transfer-out-doc-manager'));
 inventoryMap.set("inventory", require('./src/managers/inventory/inventory-manager'));
 inventoryMap.set("inventory-movement", require('./src/managers/inventory/inventory-movement-manager'));
+
 inventoryMap.set("efr-kb-rtt", require('./src/managers/inventory/efr-kb-rtt-manager'));
 inventoryMap.set("efr-kb-fng", require('./src/managers/inventory/efr-kb-fng-manager'));
 inventoryMap.set("efr-kb-rtf", require('./src/managers/inventory/efr-kb-rtf-manager'));
 inventoryMap.set("efr-kb-rtp", require('./src/managers/inventory/efr-kb-rtp-manager'));
 inventoryMap.set("efr-kb-rtd", require('./src/managers/inventory/efr-kb-rtd-manager'));
-inventoryMap.set("efr-kb-exb", require('./src/managers/inventory/efr-kb-exb-manager')); 
-
+inventoryMap.set("efr-kb-exb", require('./src/managers/inventory/efr-kb-exb-manager'));  
 inventoryMap.set("efr-tb-acc", require('./src/managers/inventory/efr-tb-acc-manager'));
 inventoryMap.set("efr-tb-bat", require('./src/managers/inventory/efr-tb-bat-manager'));
 inventoryMap.set("efr-tb-bbt", require('./src/managers/inventory/efr-tb-bbt-manager')); 
 inventoryMap.set("efr-tb-bjb", require('./src/managers/inventory/efr-tb-bjb-manager'));
 inventoryMap.set("efr-tb-bjr", require('./src/managers/inventory/efr-tb-bjr-manager'));
 inventoryMap.set("efr-tb-brd", require('./src/managers/inventory/efr-tb-brd-manager'));
-inventoryMap.set("efr-tb-brt", require('./src/managers/inventory/efr-tb-brt-manager'));
-inventoryMap.set("efr-tb-swg", require('./src/managers/inventory/efr-tb-swg-manager')); 
+inventoryMap.set("efr-tb-brt", require('./src/managers/inventory/efr-tb-brt-manager')); 
 inventoryMap.set("efr-tb-sab", require('./src/managers/inventory/efr-tb-sab-manager'));
 inventoryMap.set("efr-hp-fng", require('./src/managers/inventory/efr-hp-fng-manager'));
 
@@ -38,6 +37,19 @@ module.exports = {
         InventoryMovementManager: inventoryMap.get("inventory-movement"),
         FinishedGoodsManager: inventoryMap.get("efr-hp-fng"),
         ExpeditionsManager: inventoryMap.get("efr-kb-exb"),
+        TokoTransferStokManager: inventoryMap.get("efr-kb-rtt"),
+        FinishingKirimBarangBaruManager:inventoryMap.get("efr-kb-fng"),
+        PusatReturTokoKirimBarangReturManager:inventoryMap.get("efr-kb-rtf"),
+        TokoKirimBarangReturnManager:inventoryMap.get("efr-kb-rtp"),
+        FinishingKirimBarangReturSelesaiPerbaikanManager: inventoryMap.get("efr-kb-rtd"),
+        FinishingTerimaAksesorisManager: inventoryMap.get("efr-tb-acc"),
+        TokoTerimaAksesorisManager: inventoryMap.get("efr-tb-bat"),
+        TokoTerimaBarangBaruManager: inventoryMap.get("efr-tb-bbt"),
+        PusatBarangBaruTerimaBarangBaruManager: inventoryMap.get("efr-tb-bjb"),
+        FinishingTerimaBarangReturManager: inventoryMap.get("efr-tb-bjr"),
+        PusatReturTokoTerimaBarangReturSelesaiPerbaikanManager: inventoryMap.get("efr-tb-brd"),
+        PusatReturTokoTerimaBarangReturManager: inventoryMap.get("efr-tb-brt"), 
+        FinishingTerimaKomponenManager: inventoryMap.get("efr-tb-sab"), 
         map: inventoryMap
     },
     core: {
