4e1038e3acfea6bb4512968e4fb8221ae42aa5ba
lioenel <kepin91@yahoo.co.id>
2016-08-22 18:26:15 +0700

    add validation

--- src/managers/inventory/efr-tb-brd-manager.js
+++ src/managers/inventory/efr-tb-brd-manager.js
@@ -47,7 +47,7 @@ module.exports = class PusatReturTokoTerimaBarangReturSelesaiPerbaikanManager {
         }, paging);
 
         return new Promise((resolve, reject) => {
-            var regexModuleId = new RegExp(moduleId, "i"); 
+            var regexModuleId = new RegExp(moduleId, "i");
             var filter = {
                 _deleted: false,
                 'code': {
@@ -205,18 +205,16 @@ module.exports = class PusatReturTokoTerimaBarangReturSelesaiPerbaikanManager {
     }
 
     _validate(transferInDoc) {
+        var errors = {};
         return new Promise((resolve, reject) => {
             var valid = transferInDoc;
-            // this.moduleManager.getByCode(moduleId)
-            //     .then(module => {
-            //         var config = module.config;
-            //         valid.sourceId = config.sourceId;
-            //         valid.destinationId = config.destinationId;
-            //         resolve(valid);
-            //     })
-            //     .catch(e => {
-            //         reject(new Error(`Unable to load module:${moduleId}`));
-            //     });
+            if (!valid.reference || valid.reference == '')
+                errors["reference"] = "reference is required";
+
+            for (var prop in errors) {
+                var ValidationError = require('../../validation-error');
+                reject(new ValidationError('data does not pass validation', errors));
+            }
             resolve(valid);
         });
     }
