ac987f95d16d3e11f9f19ca81918fad58ea1b960
JanuitaM <januita.marpaung@moonlay.com>
2016-08-22 16:27:16 +0700

    repaire  unit test inventory manager

--- test/managers/inventory-manager-test.js
+++ test/managers/inventory-manager-test.js
@@ -2,27 +2,37 @@ var should = require('should');
 var helper = require('../helper');
 var validate = require('bateeq-models').validator.inventory;
 var manager;
+var testData;
 
-before('#00. connect db', function(done) {
+
+var storageId;
+var articleVariantId;
+
+before('#00. connect db', function (done) {
     helper.getDb()
         .then(db => {
-            var InventoryManager = require('../../src/managers/inventory/inventory-manager');
-            manager = new InventoryManager(db, {
-                username: 'unit-test'
-            });
-            done();
+            var data = require("../data");
+            data(db)
+                .then(result => {
+                    var InventoryManager = require('../../src/managers/inventory/inventory-manager');
+                    manager = new InventoryManager(db, {
+                        username: 'unit-test'
+                    });
+                    testData = result;
+                    storageId = testData.storages["UT-FNG"]._id.toString();
+                    articleVariantId = testData.variants["UT-AV1"]._id.toString(); 
+                    done();
+                });
         })
         .catch(e => {
             done(e);
         })
 });
 
-var storageId = '57738435e8a64fc532cd5bf1';
-var articleVariantId = '578855c4964302281454fa51';
+
 
 var createdInId;
-it('#01. should success when move-in new data', function(done) {
-
+it('#01. should success when move-in new data', function (done) { 
     manager.in(storageId, 'unit-test', articleVariantId, 10, 'remark-unit-test')
         .then(id => {
             id.should.be.Object();
@@ -35,7 +45,7 @@ it('#01. should success when move-in new data', function(done) {
 });
 
 var createdOutId;
-it('#02. should success when move-out new data', function(done) {
+it('#02. should success when move-out new data', function (done) {
 
     manager.out(storageId, 'unit-test', articleVariantId, 10, 'remark-unit-test')
         .then(id => {
@@ -48,7 +58,7 @@ it('#02. should success when move-out new data', function(done) {
         })
 });
 
-it('#03. should success when read by storageId', function(done) {
+it('#03. should success when read by storageId', function (done) {
     manager.readByStorageId(storageId)
         .then(inventories => {
             inventories.should.not.equal(null);
@@ -61,7 +71,7 @@ it('#03. should success when read by storageId', function(done) {
         })
 });
 
-it('#04. should success when read by storageId and articleVariantId', function(done) {
+it('#04. should success when read by storageId and articleVariantId', function (done) {
     manager.getByStorageIdAndArticleVarianId(storageId, articleVariantId)
         .then(inventory => {
             validate.inventory(inventory)
