03e15dfc341a6a8c6bf6b7df799adccccbc34e4d
lioenel <kepin91@yahoo.co.id>
2016-08-31 17:49:49 +0700

    add validation to BAT and BBT

--- src/managers/inventory/efr-tb-bat-manager.js
+++ src/managers/inventory/efr-tb-bat-manager.js
@@ -35,7 +35,7 @@ module.exports = class TokoTerimaAksesorisManager {
 
         var ModuleManager = require('../core/module-manager');
         this.moduleManager = new ModuleManager(db, user);
-        
+
         var SPKManager = require('../merchandiser/efr-pk-manager');
         this.spkManager = new SPKManager(db, user);
     }
@@ -55,7 +55,8 @@ module.exports = class TokoTerimaAksesorisManager {
             var deleted = {
                 _deleted: false,
                 code: {
-                        '$regex': new RegExp("^[A-Z0-9]+\/" + moduleId + "\/[0-9]{2}\/[0-9]{4}$","i")}
+                    '$regex': new RegExp("^[A-Z0-9]+\/" + moduleId + "\/[0-9]{2}\/[0-9]{4}$", "i")
+                }
             };
             var query = _paging.keyword ? {
                 '$and': [deleted]
@@ -104,11 +105,11 @@ module.exports = class TokoTerimaAksesorisManager {
 
             var regex = new RegExp("EFR\-PK/\PBA", "i");
             var filterCode = {
-                    'code': {
-                        '$regex': regex
-                    },
-                    'expeditionDocumentId' : {"$ne" : {}}
-                };
+                'code': {
+                    '$regex': regex
+                },
+                'expeditionDocumentId': { "$ne": {} }
+            };
 
             var isReceived = {
                 isReceived: false
@@ -223,11 +224,11 @@ module.exports = class TokoTerimaAksesorisManager {
                         .then(id => {
                             var reference = transferInDoc.reference;
                             this.spkManager.updateReceivedByRef(reference)
-                            .then(result => {
-                                resolve(id);
-                            }).catch(e=> {
-                                reject(e);
-                            })
+                                .then(result => {
+                                    resolve(id);
+                                }).catch(e => {
+                                    reject(e);
+                                })
                         })
                         .catch(e => {
                             reject(e);
@@ -280,44 +281,62 @@ module.exports = class TokoTerimaAksesorisManager {
         var errors = {};
         return new Promise((resolve, reject) => {
             this.spkManager.getByReference(transferInDoc.reference).
-            then(spkDoc => {
-                if(spkDoc){
-                    if(transferInDoc.password != spkDoc.password){
-                        errors["password"] = "invalid password";
+                then(spkDoc => {
+                    if (spkDoc) {
+                        if (transferInDoc.password != spkDoc.password) {
+                            errors["password"] = "invalid password";
+                        }
+                        if (transferInDoc.reference == "") {
+                            errors["reference"] = "reference is required";
+                        }
+                        if (transferInDoc.items.length <= 0) {
+                            errors["items"] = "no item(s) to transfer in";
+                        }
+                        if (spkDoc.sourceId.toString() != transferInDoc.sourceId.toString()) {
+                            errors["sourceId"] = "invalid sourceId";
+                        }
+                        if (spkDoc.destinationId.toString() != transferInDoc.destinationId.toString()) {
+                            errors["destinationId"] = "invalid destinationId";
+                        }
+                        if (spkDoc.isReceived) {
+                            errors["isReceived"] = "this reference already received";
+                        }
+                        var index = 0;
+                        var itemErrors = [];
+                        for (var item of transferInDoc.items) {
+                            var itemError = {};
+                            if (item.quantity <= 0)
+                                itemError["quantity"] = "items should not contains 0 quantity";
+                            else
+                                if (item.quantity != spkDoc.items[index].quantity)
+                                    if (item.remark == "")
+                                        itemError["remark"] = "Masukkan no referensi berita acara"; 
+                            index++;
+                            itemErrors.push(itemError);
+                        }
+                        for (var itemError of itemErrors) {
+                            for (var prop in itemError) {
+                                errors.items = itemErrors;
+                                break;
+                            }
+                            if (errors.items)
+                                break;
+                        }
+                        for (var prop in errors) {
+                            var ValidationError = require('../../validation-error');
+                            reject(new ValidationError('data does not pass validation', errors));
+                        }
                     }
-                    if(transferInDoc.reference == ""){
-                        errors["reference"] = "reference is required";
-                    }
-                    if(transferInDoc.items.length <= 0){
-                        errors["items"] = "no item(s) to transfer in";
-                    }
-                    if(spkDoc.sourceId.toString() != transferInDoc.sourceId.toString()){
-                        errors["sourceId"] = "invalid sourceId";
-                    }
-                    if(spkDoc.destinationId.toString() != transferInDoc.destinationId.toString()){
-                        errors["destinationId"] = "invalid destinationId";
-                    }
-                    if(spkDoc.isReceived){
-                        errors["isReceived"] = "this reference already received";
-                    }
-                    for(var item of transferInDoc.items){
-                        if(item.quantity <= 0)
-                            errors["items"] = "items should not contains 0 quantity";
-                    }
-                    for (var prop in errors) {
+                    else {
+                        errors["reference"] = "reference not found";
                         var ValidationError = require('../../validation-error');
                         reject(new ValidationError('data does not pass validation', errors));
                     }
-                }else{
-                    errors["reference"] = "reference not found";
-                    var ValidationError = require('../../validation-error');
-                    reject(new ValidationError('data does not pass validation', errors));
-                }
-                resolve(transferInDoc);
-            })
-            .catch(e=>{
-                reject(e);
-            })
+                    resolve(transferInDoc);
+                })
+                .catch(e => {
+                    reject(e);
+                })
         });
     }
 };
--- src/managers/inventory/efr-tb-bbt-manager.js
+++ src/managers/inventory/efr-tb-bbt-manager.js
@@ -52,8 +52,8 @@ module.exports = class TokoTerimaBarangBaruManager {
             var deleted = {
                 _deleted: false,
                 code: {
-                        '$regex': new RegExp("^[A-Z0-9]+\/" + moduleId + "\/[0-9]{2}\/[0-9]{4}$","i")
-                    }
+                    '$regex': new RegExp("^[A-Z0-9]+\/" + moduleId + "\/[0-9]{2}\/[0-9]{4}$", "i")
+                }
             };
             var query = _paging.keyword ? {
                 '$and': [deleted]
@@ -68,7 +68,7 @@ module.exports = class TokoTerimaBarangBaruManager {
                 };
                 var $or = {
                     '$or': [filterCode]
-                }; 
+                };
                 query['$and'].push($or);
             }
 
@@ -86,7 +86,7 @@ module.exports = class TokoTerimaBarangBaruManager {
         });
     }
 
-    readPendingSPK(paging){
+    readPendingSPK(paging) {
         var _paging = Object.assign({
             page: 1,
             size: 20,
@@ -98,24 +98,26 @@ module.exports = class TokoTerimaBarangBaruManager {
             var deleted = {
                 _deleted: false
             };
-            
-            var regex = new RegExp("EFR\-PK/\PBJ|EFR\-PK/\PBR","i");
-            var filterCode = {
-                    'code': {
-                        '$regex': regex
-                    },
-                    'expeditionDocumentId' : {"$ne" : {}}
-                };
 
-            var isReceived = {
-                isReceived : false
+            var regex = new RegExp("EFR\-PK/\PBJ|EFR\-PK/\PBR", "i");
+            var filterCode = {
+                'code': {
+                    '$regex': regex
+                },
+                'expeditionDocumentId': { "$ne": {} }
             };
 
-            var query = {$and:[
-                deleted,
-                filterCode,
-                isReceived
-            ]}
+            var isReceived = {
+                isReceived: false
+            };
+
+            var query = {
+                $and: [
+                    deleted,
+                    filterCode,
+                    isReceived
+                ]
+            }
 
             this.spkDocCollection
                 .where(query)
@@ -190,12 +192,12 @@ module.exports = class TokoTerimaBarangBaruManager {
         })
     }
 
-    getPendingSPKById(id){
+    getPendingSPKById(id) {
         return new Promise((resolve, reject) => {
             var query = {
                 _id: new ObjectId(id),
                 _deleted: false,
-                isReceived:false
+                isReceived: false
             };
             this.spkDocCollection.singleOrDefault(query)
                 .then(SPKDoc => {
@@ -208,25 +210,25 @@ module.exports = class TokoTerimaBarangBaruManager {
                 });
         });
     }
-    
+
     create(transferInDoc) {
         return new Promise((resolve, reject) => {
             this._validate(transferInDoc)
-                .then(validTransferInDoc => { 
+                .then(validTransferInDoc => {
                     validTransferInDoc.code = generateCode(moduleId);
-                            this.transferInDocManager.create(validTransferInDoc)
-                                .then(id => { 
-                                    var reference = transferInDoc.reference;
-                                    this.spkManager.updateReceivedByRef(reference)
-                                    .then(result => {
-                                        resolve(id);
-                                    }).catch(e=> {
-                                        reject(e);
-                                    });
-                                })
-                            .catch(e => {
-                                reject(e);
-                            })
+                    this.transferInDocManager.create(validTransferInDoc)
+                        .then(id => {
+                            var reference = transferInDoc.reference;
+                            this.spkManager.updateReceivedByRef(reference)
+                                .then(result => {
+                                    resolve(id);
+                                }).catch(e => {
+                                    reject(e);
+                                });
+                        })
+                        .catch(e => {
+                            reject(e);
+                        })
                 })
                 .catch(e => {
                     reject(e);
@@ -275,44 +277,62 @@ module.exports = class TokoTerimaBarangBaruManager {
         var errors = {};
         return new Promise((resolve, reject) => {
             this.spkManager.getByReference(transferInDoc.reference).
-            then(spkDoc => {
-                if(spkDoc){
-                    if(transferInDoc.password != spkDoc.password){
-                        errors["password"] = "invalid password";
+                then(spkDoc => {
+                    if (spkDoc) {
+                        if (transferInDoc.password != spkDoc.password) {
+                            errors["password"] = "invalid password";
+                        }
+                        if (transferInDoc.reference == "") {
+                            errors["reference"] = "reference is required";
+                        }
+                        if (transferInDoc.items.length <= 0) {
+                            errors["items"] = "no item(s) to transfer in";
+                        }
+                        if (spkDoc.sourceId.toString() != transferInDoc.sourceId.toString()) {
+                            errors["sourceId"] = "invalid sourceId";
+                        }
+                        if (spkDoc.destinationId.toString() != transferInDoc.destinationId.toString()) {
+                            errors["destinationId"] = "invalid destinationId";
+                        }
+                        if (spkDoc.isReceived) {
+                            errors["isReceived"] = "this reference already received";
+                        }
+                        var index = 0;
+                        var itemErrors = [];
+                        for (var item of transferInDoc.items) {
+                            var itemError = {};
+                            if (item.quantity <= 0)
+                                itemError["quantity"] = "items should not contains 0 quantity";
+                            else
+                                if (item.quantity != spkDoc.items[index].quantity)
+                                    if (item.remark == "")
+                                        itemError["remark"] = "Masukkan no referensi berita acara";
+                            index++;
+                            itemErrors.push(itemError);
+                        }
+                        for (var itemError of itemErrors) {
+                            for (var prop in itemError) {
+                                errors.items = itemErrors;
+                                break;
+                            }
+                            if (errors.items)
+                                break;
+                        }
+                        for (var prop in errors) {
+                            var ValidationError = require('../../validation-error');
+                            reject(new ValidationError('data does not pass validation', errors));
+                        }
                     }
-                    if(transferInDoc.reference == ""){
-                        errors["reference"] = "reference is required";
-                    }
-                    if(transferInDoc.items.length <= 0){
-                        errors["items"] = "no item(s) to transfer in";
-                    }
-                    if(spkDoc.sourceId.toString() != transferInDoc.sourceId.toString()){
-                        errors["sourceId"] = "invalid sourceId";
-                    }
-                    if(spkDoc.destinationId.toString() != transferInDoc.destinationId.toString()){
-                        errors["destinationId"] = "invalid destinationId";
-                    }
-                    if(spkDoc.isReceived){
-                        errors["isReceived"] = "this reference already received";
-                    }
-                    for(var item of transferInDoc.items){
-                        if(item.quantity <= 0)
-                            errors["items"] = "items should not contains 0 quantity";
-                    }
-                    for (var prop in errors) {
+                    else {
+                        errors["reference"] = "reference not found";
                         var ValidationError = require('../../validation-error');
                         reject(new ValidationError('data does not pass validation', errors));
                     }
-                }else{
-                    errors["reference"] = "reference not found";
-                    var ValidationError = require('../../validation-error');
-                    reject(new ValidationError('data does not pass validation', errors));
-                }
-                resolve(transferInDoc);
-            })
-            .catch(e=>{
-                reject(e);
-            })
+                    resolve(transferInDoc);
+                })
+                .catch(e => {
+                    reject(e);
+                })
         });
-    } 
+    }
 };
