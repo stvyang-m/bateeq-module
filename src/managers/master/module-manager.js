616b7ea5061b8d79fe3ed673ae543bf36b5aca53
Tris Setiawan <tris.setiawan@yahoo.com>
2016-11-17 07:47:03 +0000

    fix ModuleManager

--- src/managers/master/module-manager.js
+++ src/managers/master/module-manager.js
@@ -5,37 +5,28 @@ var ObjectId = require('mongodb').ObjectId;
 
 // internal deps
 require('mongodb-toolkit');
+var BaseManager = require('../base-manager');
 var BateeqModels = require('bateeq-models');
 var map = BateeqModels.map;
 
 var Module = BateeqModels.master.Module;
 
 
-module.exports = class ModuleManager {
+module.exports = class ModuleManager extends BaseManager {
     constructor(db, user) {
-        this.db = db;
-        this.user = user;
-        this.moduleCollection = this.db.use(map.master.Module);
-    }
+        super(db, user);
+        this.collection = this.db.use(map.master.Module); 
+    } 
 
-    read(paging) {
-        var _paging = Object.assign({
-            page: 1,
-            size: 20,
-            order: '_id',
-            asc: true
-        }, paging);
+    _getQuery(paging) {
+        var deletedFilter = {
+            _deleted: false
+        }, keywordFilter = {};
+        
+        var query = {};
 
-        return new Promise((resolve, reject) => {
-            var deleted = {
-                _deleted: false
-            };
-            var query = _paging.keyword ? {
-                '$and': [deleted]
-            } : deleted;
-
-            if (_paging.keyword) {
-                var regex = new RegExp(_paging.keyword, "i");
+        if (paging.keyword) {
+            var regex = new RegExp(paging.keyword, "i");
                 var filterCode = {
                     'code': {
                         '$regex': regex
@@ -46,170 +37,20 @@ module.exports = class ModuleManager {
                         '$regex': regex
                     }
                 };
-                var $or = {
-                    '$or': [filterCode, filterName]
-                };
-
-                query['$and'].push($or);
-            }
-
-
-            this.moduleCollection
-                .where(query)
-                .page(_paging.page, _paging.size)
-                .orderBy(_paging.order, _paging.asc)
-                .execute()
-                .then(modules => {
-                    resolve(modules);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        });
+            keywordFilter = {
+                '$or': [filterCode, filterName]
+            }; 
+        }
+        query = { '$and': [deletedFilter, paging.filter, keywordFilter] };
+        return query;
     }
 
-    getSingleById(id) {
-        return new Promise((resolve, reject) => {
-            if (id === '')
-                resolve(null);
-            var query = {
-                _id: new ObjectId(id),
-                _deleted: false
-            };
-            this.getSingleByQuery(query)
-                .then(module => {
-                    resolve(module);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        });
-    }
-
-    getByCode(code) {
-        return new Promise((resolve, reject) => {
-            if (code === '')
-                resolve(null);
-            var query = {
-                code: code,
-                _deleted: false
-            };
-            this.getSingleByQuery(query)
-                .then(module => {
-                    resolve(module);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        });
-    }
-
-    getSingleByIdOrDefault(id) {
-        return new Promise((resolve, reject) => {
-            if (id === '')
-                resolve(null);
-            var query = {
-                _id: new ObjectId(id),
-                _deleted: false
-            };
-            this.getSingleByQueryOrDefault(query)
-                .then(module => {
-                    resolve(module);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        });
-    }
-
-    getSingleByQuery(query) {
-        return new Promise((resolve, reject) => {
-            this.moduleCollection
-                .single(query)
-                .then(module => {
-                    resolve(module);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        })
-    }
-
-    getSingleByQueryOrDefault(query) {
-        return new Promise((resolve, reject) => {
-            this.moduleCollection
-                .singleOrDefault(query)
-                .then(module => {
-                    resolve(module);
-                })
-                .catch(e => {
-                    reject(e);
-                });
-        })
-    }
-
-    create(module) {
-        return new Promise((resolve, reject) => {
-            this._validate(module)
-                .then(validModule => {
-
-                    this.moduleCollection.insert(validModule)
-                        .then(id => {
-                            resolve(id);
-                        })
-                        .catch(e => {
-                            reject(e);
-                        })
-                })
-                .catch(e => {
-                    reject(e);
-                })
-        });
-    }
-
-    update(module) {
-        return new Promise((resolve, reject) => {
-            this._validate(module)
-                .then(validModule => {
-                    this.moduleCollection.update(validModule)
-                        .then(id => {
-                            resolve(id);
-                        })
-                        .catch(e => {
-                            reject(e);
-                        })
-                })
-                .catch(e => {
-                    reject(e);
-                })
-        });
-    }
-
-    delete(module) {
-        return new Promise((resolve, reject) => {
-            this._validate(module)
-                .then(validModule => {
-                    validModule._deleted = true;
-                    this.moduleCollection.update(validModule)
-                        .then(id => {
-                            resolve(id);
-                        })
-                        .catch(e => {
-                            reject(e);
-                        })
-                })
-                .catch(e => {
-                    reject(e);
-                })
-        });
-    }
- 
     _validate(module) {
-        var errors = {};
+         var errors = {};
         return new Promise((resolve, reject) => {
             var valid = new Module(module);
             // 1. begin: Declare promises.
-            var getModule = this.moduleCollection.singleOrDefault({
+            var getModule = this.collection.singleOrDefault({
                 "$and": [{
                     _id: {
                         '$ne': new ObjectId(valid._id)
@@ -247,5 +88,5 @@ module.exports = class ModuleManager {
                     reject(e);
                 })
         });
-    }
+    } 
 };
--- test/index.js
+++ test/index.js
@@ -11,9 +11,9 @@ describe('#bateeq-module', function(done) {
     // test('@auth/account-manager', './auth/account-manager-test');
     // test('@auth/role-manager', './auth/role-manager-test');
     
-    test('@manager/master/item-manager', './managers/master/item-manager-test');
-    test('@manager/master/material-manager', './managers/master/material-manager-test');
-    test('@manager/master/finished-goods-manager', './managers/master/finished-goods-manager-test');
+    // test('@manager/master/item-manager', './managers/master/item-manager-test');
+    // test('@manager/master/material-manager', './managers/master/material-manager-test');
+    // test('@manager/master/finished-goods-manager', './managers/master/finished-goods-manager-test');
     
     // test('@manager/article-brand-manager', './managers/article-brand-manager-test');
     // test('@manager/article-category-manager', './managers/article-category-manager-test');
@@ -27,12 +27,12 @@ describe('#bateeq-module', function(done) {
     // test('@manager/article-type-manager', './managers/article-type-manager-test');
     // test('@manager/article-manager', './managers/article-manager-test');
     
-    test('@manager/storage-manager', './managers/storage-manager-test');
-    test('@manager/store-manager', './managers/store-manager-test');
-    test('@manager/transfer-in-doc-manager', './managers/transfer-in-doc-manager-test');
-    test('@manager/transfer-out-doc-manager', './managers/transfer-out-doc-manager-test');
-    test('@manager/transfer-in-doc-ext-manager', './managers/transfer-in-doc-ext-manager-test');
-    test('@manager/inventory-manager', './managers/inventory-manager-test');
+    // test('@manager/storage-manager', './managers/storage-manager-test');
+    // test('@manager/store-manager', './managers/store-manager-test');
+    // test('@manager/transfer-in-doc-manager', './managers/transfer-in-doc-manager-test');
+    // test('@manager/transfer-out-doc-manager', './managers/transfer-out-doc-manager-test');
+    // test('@manager/transfer-in-doc-ext-manager', './managers/transfer-in-doc-ext-manager-test');
+    // test('@manager/inventory-manager', './managers/inventory-manager-test');
     test('@manager/module-manager', './managers/module-manager-test');
     
     // test('@manager/efr-kb-rtt-manager', './managers/efr-kb-rtt-manager-test');
@@ -56,8 +56,8 @@ describe('#bateeq-module', function(done) {
     // test('@manager/efr-hp-fng-manager', './managers/efr-hp-fng-manager-test'); 
     // test('@manager/supplier-manager', './managers/supplier-manager-test');
     
-    test('@manager/bank-manager', './managers/bank-manager-test'); 
-    test('@manager/card-type-manager', './managers/card-type-manager-test'); 
+    // test('@manager/bank-manager', './managers/bank-manager-test'); 
+    // test('@manager/card-type-manager', './managers/card-type-manager-test'); 
     // test('@manager/promo-manager', './managers/promo-manager-test');
-    test('@manager/sales-manager', './managers/sales-manager-test');
+    // test('@manager/sales-manager', './managers/sales-manager-test');
 })
