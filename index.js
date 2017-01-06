ed5785747b6d2b7fe4d6a722fd2496c01c52745d
adityahenanda <aditya.henanda@MarcJacobs.moonlay.jakarta>
2017-01-06 21:18:08 +0700

    Revert "etl"
    This reverts commit f57f37486c352119889638b119327c08c5e202b0.

--- index.js
+++ index.js
@@ -87,9 +87,7 @@ module.exports = {
         itemsSpMigration:require('./src/etl/sp-items-etl'),
         storesMigration:require('./src/etl/stores-migration-etl'),
         storagesMigration:require('./src/etl/storages-migration-etl'),
-        salesMigrationSp:require('./src/etl/sp-sales-etl-test'),
+        salesMigration:require('./src/etl/sales-etl'),
         salesSpMigration:require('./src/etl/sp-sales-etl'),
-        
-        
     } 
 }
