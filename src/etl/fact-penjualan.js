b362b5ba0c16d07a65399731a2f4b88125d5f203
danielmoonlay <daniel.nababan@moonlay.com>
2017-03-03 11:09:16 +0700

    update filter logic
    - add filter code contains 'sales' only
    - add total items loaded information

--- src/etl/fact-penjualan.js
+++ src/etl/fact-penjualan.js
@@ -56,7 +56,8 @@ module.exports = class FactPenjualan {
                                     summary: {
                                         extract: moment(exctractDate).diff(moment(startedDate), "minutes") + " minutes",
                                         transform: moment(transformDate).diff(moment(exctractDate), "minutes") + " minutes",
-                                        load: moment(finishedDate).diff(moment(transformDate), "minutes") + " minutes"
+                                        load: moment(finishedDate).diff(moment(transformDate), "minutes") + " minutes",
+                                        items : `${result.count} items`
                                     }
                                 };
                                 this.migrationLog.updateOne({ _createdDate: startedDate }, updateLog);
@@ -128,7 +129,8 @@ module.exports = class FactPenjualan {
             _deleted: false,
             _updatedDate: {
                 "$gt": timestamp
-            }
+            },
+            "code": { "$regex": new RegExp("/.*sales.*/") }
         }).toArray();
     }
 
@@ -338,19 +340,21 @@ module.exports = class FactPenjualan {
                         var command = [];
 
                         var sqlQuery = '';
+                        var allSqlQuery = '';
 
-                        var count = 1;
+                        var count = 0;
 
                         for (var item of data) {
                             if (item) {
+                                count++;
                                 var queryString = `INSERT INTO [BTQ_FactPenjualan_Temp] ([timekey],[countdays],[store_code],[hd_transaction_number],[hd_shift],[hd_subtotal],[hd_sales_discount_percentage],[hd_grandtotal],[hd_payment_type],[hd_card],[hd_card_type],[hd_bank_name],[hd_card_number],[hd_voucher_amount],[hd_cash_amount],[hd_card_amount],[dt_item_name],[dt_item_quantity],[dt_barcode],[dt_motif_name],[dt_counter_name],[dt_subcounter_name],[dt_material_name],[dt_size_name],[dt_mainsalesprice],[dt_item_price],[dt_is_discount_percentage],[dt_fixed_discount_amount],[dt_discount_product_percentage],[dt_discount_product_percentage_additional],[dt_special_discount_product_percentage],[dt_margin_percentage],[dt_total_price_per_product],[store_name],[store_city],[store_open_date],[store_close_date],[store_area],[store_status],[store_wide],[store_offline_online],[store_sales_category],[store_monthly_total_cost],[store_category],[store_montly_omzet_target],[hd_pos],[hd_bank_card],[hd_is_void]) values(${item.timekey},${item.countdays},${item.store_code},${item.hd_transaction_number},${item.hd_shift},${item.hd_subtotal},${item.hd_sales_discount_percentage},${item.hd_grandtotal},${item.hd_payment_type},${item.hd_card},${item.hd_card_type},${item.hd_bank_name},${item.hd_card_number},${item.hd_voucher_amount},${item.hd_cash_amount},${item.hd_card_amount},${item.dt_item_name},${item.dt_item_quantity},${item.dt_barcode},${item.dt_motif_name},${item.dt_counter_name},${item.dt_subcounter_name},${item.dt_material_name},${item.dt_size_name},${item.dt_mainsalesprice},${item.dt_item_price},${item.dt_is_discount_percentage},${item.dt_fixed_discount_amount},${item.dt_discount_product_percentage},${item.dt_discount_product_percentage_additional},${item.dt_special_discount_product_percentage},${item.dt_margin_percentage},${item.dt_total_price_per_product},${item.store_name},${item.store_city},${item.store_open_date},${item.store_close_date},${item.store_area},${item.store_status},${item.store_wide},${item.store_offline_online},${item.store_sales_category},${item.store_monthly_total_cost},${item.store_category},${item.store_montly_omzet_target},${item.hd_pos},${item.hd_bank_card},${item.hd_is_void})\n`;
                                 sqlQuery = sqlQuery.concat(queryString);
+                                allSqlQuery = allSqlQuery.concat(queryString);
                                 if (count % 1000 == 0) {
                                     command.push(this.insertQuery(request, sqlQuery));
                                     sqlQuery = "";
                                 }
                                 // console.log(`add data to query  : ${count}`);
-                                count++;
                             }
                         }
 
@@ -359,16 +363,16 @@ module.exports = class FactPenjualan {
 
                         request.multiple = true;
 
-                        // var fs = require("fs");
-                        // var path = "C:\\Users\\daniel.nababan.MOONLAY\\Desktop\\sqlQuery.txt";
+                        var fs = require("fs");
+                        var path = "C:\\Users\\daniel.nababan.MOONLAY\\Desktop\\sqlQuery.txt";
 
-                        // fs.writeFile(path, sqlQuery, function (error) {
-                        //     if (error) {
-                        //         console.log("write error:  " + error.message);
-                        //     } else {
-                        //         console.log("Successful Write to " + path);
-                        //     }
-                        // });
+                        fs.writeFile(path, allSqlQuery, function (error) {
+                            if (error) {
+                                console.log("write error:  " + error.message);
+                            } else {
+                                console.log("Successful Write to " + path);
+                            }
+                        });
 
                         return Promise.all(command)
                             .then((results) => {
@@ -385,7 +389,7 @@ module.exports = class FactPenjualan {
                                             if (err)
                                                 reject(err);
                                             else
-                                                resolve(results);
+                                                resolve({"results" : results, "count" : count});
                                         });
                                     }).catch((error) => {
                                         transaction.rollback((err) => {
