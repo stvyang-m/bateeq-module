4191d6ae90b7435265e895fdd0acdde79a723d4c
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-01-15 10:37:16 +0700

    update test for purchase-request only using basic test

--- test/managers/purchasing/purchase-request/index.js
+++ test/managers/purchasing/purchase-request/index.js
@@ -2,22 +2,22 @@ describe("BASIC CRUD SCENARIOS", function() {
     require("./basic");
 });
 
-describe("CREATE SCENARIOS", function() {
-    require("./create");
-});
+// describe("CREATE SCENARIOS", function() {
+//     require("./create");
+// });
 
-describe("POST SCENARIOS", function() {
-    require("./post");
-});
+// describe("POST SCENARIOS", function() {
+//     require("./post");
+// });
 
-describe("UNPOST SCENARIOS", function() {
-    require("./unpost");
-});
+// describe("UNPOST SCENARIOS", function() {
+//     require("./unpost");
+// });
 
 // describe("REPORT SCENARIOS", function() {
 //     require("./report");
 // });
 
-describe("PDF SCENARIOS", function() {
-    require("./pdf");
-});
+// describe("PDF SCENARIOS", function() {
+//     require("./pdf");
+// });
