b573cedb73e7c9cb24d0d70e26581c64dea52f5a
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-01-16 17:42:07 +0700

    update code generator

--- src/utils/code-generator.js
+++ src/utils/code-generator.js
@@ -2,16 +2,13 @@ var ObjectId = require('mongodb').ObjectId;
 var Hashids = require("hashids");
 var moment = require("moment");
 
-module.exports = function(moduleId) {
+module.exports = function(context) {
 
-
-    var salt = new ObjectId().toString();
-
-    var hashids = new Hashids(salt, 8, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+    var salt = context ? context : new ObjectId().toString();
+    var hashids = new Hashids(salt, 8, "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789");
     var now = moment();
     var begin = now.clone().startOf("month");
     var diff = now.diff(begin);
-    var code = `${hashids.encode(diff)}/${moduleId}/${now.format("MM/YYYY")}`;
-
+    var code = `${hashids.encode(diff)}`;
     return code;
 }
