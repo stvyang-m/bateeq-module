e68468897f31c80b99105d6ebaa8ac1320185d28
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-01-17 11:43:28 +0700

    update purchase-request test/index

--- test/managers/purchasing/purchase-request/index.js
+++ test/managers/purchasing/purchase-request/index.js
@@ -1,23 +1,3 @@
 describe("BASIC CRUD SCENARIOS", function() {
     require("./basic");
 });
-
-// describe("CREATE SCENARIOS", function() {
-//     require("./create");
-// });
-
-// describe("POST SCENARIOS", function() {
-//     require("./post");
-// });
-
-// describe("UNPOST SCENARIOS", function() {
-//     require("./unpost");
-// });
-
-// describe("REPORT SCENARIOS", function() {
-//     require("./report");
-// });
-
-// describe("PDF SCENARIOS", function() {
-//     require("./pdf");
-// });
