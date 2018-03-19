7fd1792acfefb9b2c914135c7b92b6818d14302a
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-03-19 11:55:05 +0700

    update new test

--- test/managers/purchasing/purchase-order/new-test.js
+++ test/managers/purchasing/purchase-order/new-test.js
@@ -1,19 +1,21 @@
-'esversion: 6'
+'esversion:6';
 
 var should = require("should");
 var helper = require("../../../helper");
-var PurchaseOrderManager = require("../../../../src/managers/purchasing/purchase-order-manager");
-var PurchaseRequestManager = require("../../../../src/managers/purchasing/purchase-request-manager");
+
 var purchaseRequestDataUtil = require("../../../data-util/purchasing/purchase-request-data-util");
-var purchaseOrderDataUtil = require("../../../data-util/purchasing/purchase-order-data-util");
-var purchaseOrderManager;
-var purchaseRequestManager;
+var validatePR = require("bateeq-models").validator.purchasing.purchaseRequest;
+var PurchaseRequestManager = require("../../../../src/managers/purchasing/purchase-request-manager");
+var purchaseRequestManager = null;
 var purchaseRequest;
-var purchaseOrderFromDataUtil;
-var purchaseOrderId;
+
+var purchaseOrderDataUtil = require("../../../data-util/purchasing/purchase-order-data-util");
+var validatePO = require("bateeq-models").validator.purchasing.purchaseOrder;
+var PurchaseOrderManager = require("../../../../src/managers/purchasing/purchase-order-manager");
+var purchaseOrderManager = null;
 var purchaseOrder;
 
-before('#00. connect db & create instance', function (done) {
+before('#00. connect db', function (done) {
     helper.getDb()
         .then(db => {
             purchaseRequestManager = new PurchaseRequestManager(db, {
@@ -22,7 +24,6 @@ before('#00. connect db & create instance', function (done) {
             purchaseOrderManager = new PurchaseOrderManager(db, {
                 username: 'dev'
             });
-
             done();
         })
         .catch(e => {
@@ -30,18 +31,18 @@ before('#00. connect db & create instance', function (done) {
         });
 });
 
-
-it('#01. Create Purchase Request', function (done) {
+it('#01. should success when create new purchase-order from data util', function (done) {
     purchaseRequestDataUtil.getNewTestData()
-        .then(data => {
-            purchaseRequest = data;
-
+        .then(pr => {
+            purchaseRequest = pr;
+            validatePR(purchaseRequest);
             done();
         })
         .catch(e => {
             done(e);
         });
 });
+
 
 it('#02. should failed when create new purchase-order with unposted purchase-request', function (done) {
     purchaseOrderDataUtil.getNewData(purchaseRequest)
@@ -57,13 +58,19 @@ it('#02. should failed when create new purchase-order with unposted purchase-req
         });
 });
 
-it('#03. should success when get new purchase-order data from util with posted purchase-request', function (done) {
+it('#02. should success when create new purchase-order with posted purchase-request', function (done) {
     purchaseRequestManager.post([purchaseRequest])
         .then(pr => {
             purchaseRequest = pr[0];
             purchaseOrderDataUtil.getNewData(purchaseRequest)
-                .then((data) => {
-                    purchaseOrderFromDataUtil = data;
+                .then((purchaseOrder) => {
+                    return purchaseOrderManager.create(purchaseOrder);
+                })
+                .then((id) => {
+                    return purchaseOrderManager.getSingleById(id);
+                })
+                .then(po => {
+                    purchaseOrder = po;
                     done();
                 })
                 .catch(e => {
@@ -74,26 +81,4 @@ it('#03. should success when get new purchase-order data from util with posted p
         .catch(e => {
             done(e);
         });
-});
-
-it('#04. should success when create new purchase-order with posted purchase-request', function (done) {
-    purchaseOrderManager.create(purchaseOrderFromDataUtil)
-        .then(id => {
-            purchaseOrderId = id;
-            done();
-        })
-        .catch(e => {
-            done(e);
-        });
-});
-
-it('#05. should success when validate purchase-order with posted purchase-request', function (done) {
-    purchaseOrderManager.getSingleById(purchaseOrderId)
-    .then(data => {
-        purchaseOrder = data;
-        done();
-    })
-    .catch(e => {
-        done(e);
-    });
 });
