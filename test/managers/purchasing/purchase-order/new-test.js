8e8332d27c3f8cee67e7dd98f20115d2da8258dd
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-03-19 12:15:29 +0700

    update new test

--- test/managers/purchasing/purchase-order/new-test.js
+++ test/managers/purchasing/purchase-order/new-test.js
@@ -1,4 +1,4 @@
-'esversion:6';
+/*jshint esversion: 6 */
 
 var should = require("should");
 var helper = require("../../../helper");
@@ -7,13 +7,12 @@ var purchaseRequestDataUtil = require("../../../data-util/purchasing/purchase-re
 var validatePR = require("bateeq-models").validator.purchasing.purchaseRequest;
 var PurchaseRequestManager = require("../../../../src/managers/purchasing/purchase-request-manager");
 var purchaseRequestManager = null;
-var purchaseRequest;
-
 var purchaseOrderDataUtil = require("../../../data-util/purchasing/purchase-order-data-util");
 var validatePO = require("bateeq-models").validator.purchasing.purchaseOrder;
 var PurchaseOrderManager = require("../../../../src/managers/purchasing/purchase-order-manager");
 var purchaseOrderManager = null;
 var purchaseOrder;
+var purchaseRequest;
 
 before('#00. connect db', function (done) {
     helper.getDb()
@@ -25,58 +24,6 @@ before('#00. connect db', function (done) {
                 username: 'dev'
             });
             done();
-        })
-        .catch(e => {
-            done(e);
-        });
-});
-
-it('#01. should success when create new purchase-order from data util', function (done) {
-    purchaseRequestDataUtil.getNewTestData()
-        .then(pr => {
-            purchaseRequest = pr;
-            validatePR(purchaseRequest);
-            done();
-        })
-        .catch(e => {
-            done(e);
-        });
-});
-
-
-it('#02. should failed when create new purchase-order with unposted purchase-request', function (done) {
-    purchaseOrderDataUtil.getNewData(purchaseRequest)
-        .then((purchaseOrder) => {
-            return purchaseOrderManager.create(purchaseOrder);
-        })
-        .then(po => {
-            done(purchaseRequest, "purchase-request cannot be used to create purchase-order due unposted status");
-        })
-        .catch(e => {
-            e.errors.should.have.property('purchaseRequestId');
-            done();
-        });
-});
-
-it('#02. should success when create new purchase-order with posted purchase-request', function (done) {
-    purchaseRequestManager.post([purchaseRequest])
-        .then(pr => {
-            purchaseRequest = pr[0];
-            purchaseOrderDataUtil.getNewData(purchaseRequest)
-                .then((purchaseOrder) => {
-                    return purchaseOrderManager.create(purchaseOrder);
-                })
-                .then((id) => {
-                    return purchaseOrderManager.getSingleById(id);
-                })
-                .then(po => {
-                    purchaseOrder = po;
-                    done();
-                })
-                .catch(e => {
-                    done(e);
-                });
-
         })
         .catch(e => {
             done(e);
